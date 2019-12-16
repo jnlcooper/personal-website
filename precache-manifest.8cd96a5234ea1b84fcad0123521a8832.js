bc5eda98c4c46adbe9dbeb67ea222108abae161e
Joanna Cooper <joannacooper95@hotmail.co.uk>
2019-12-16 09:26:33 -0500

    Updates

---
+++ precache-manifest.8cd96a5234ea1b84fcad0123521a8832.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e04b76098afb6a89c608ca148eca149d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccae70060626ad7a15b0",
+    "url": "/static/css/main.8a1cfd67.chunk.css"
+  },
+  {
+    "revision": "da9792e368ba1a91e686",
+    "url": "/static/js/2.1ae3e124.chunk.js"
+  },
+  {
+    "revision": "ccae70060626ad7a15b0",
+    "url": "/static/js/main.a50a2076.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8f060295de9fe75f1563d0b77a1986e8",
+    "url": "/static/media/christmascard2015_small.8f060295.jpg"
+  },
+  {
+    "revision": "476f3fd69b5751a0ecd4c8807caf0be7",
+    "url": "/static/media/contact_illustration.476f3fd6.png"
+  },
+  {
+    "revision": "5fcf56fbb80dc36ed86c585407a6beb9",
+    "url": "/static/media/hamburger_hover_icon.5fcf56fb.svg"
+  },
+  {
+    "revision": "86e6ebd01568b41a8a2b2dde581e519c",
+    "url": "/static/media/hamburger_icon.86e6ebd0.svg"
+  },
+  {
+    "revision": "9b53a2171793678875e5ad7d51c70b5f",
+    "url": "/static/media/juniorsjourneybanner.9b53a217.png"
+  },
+  {
+    "revision": "1f2471a4676650d90d26699c9b1e2996",
+    "url": "/static/media/logo.1f2471a4.svg"
+  },
+  {
+    "revision": "9daa63f6bbc03186fe4deafbea55ad31",
+    "url": "/static/media/logo_grey.9daa63f6.svg"
+  },
+  {
+    "revision": "71b80df53547046a953a38cb795e4699",
+    "url": "/static/media/parliamentDrawing.71b80df5.png"
+  },
+  {
+    "revision": "9efd0caace424db30d9a17a81a475475",
+    "url": "/static/media/portrait.9efd0caa.jpg"
+  },
+  {
+    "revision": "0f3472609de9264785bea5128b202161",
+    "url": "/static/media/profile_pic_opacity.0f347260.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.f188c5913214816c57956b8b928c2408.js"
+  "/precache-manifest.8cd96a5234ea1b84fcad0123521a8832.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
