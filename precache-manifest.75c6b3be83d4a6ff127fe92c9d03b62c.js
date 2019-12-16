61bdeef8812407c4b65151cf8b1f4bfe53696678
Joanna Cooper <joannacooper95@hotmail.co.uk>
2019-12-15 19:08:42 -0500

    Updates

---
+++ precache-manifest.75c6b3be83d4a6ff127fe92c9d03b62c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248dc73d53837e34c09dddecee160d71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90e7f663a24fb12ce977",
+    "url": "/static/css/main.3733c35f.chunk.css"
+  },
+  {
+    "revision": "da9792e368ba1a91e686",
+    "url": "/static/js/2.1ae3e124.chunk.js"
+  },
+  {
+    "revision": "90e7f663a24fb12ce977",
+    "url": "/static/js/main.0ef350f4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8f060295de9fe75f1563d0b77a1986e8",
+    "url": "/static/media/christmascard2015_small.8f060295.jpg"
+  },
+  {
+    "revision": "476f3fd69b5751a0ecd4c8807caf0be7",
+    "url": "/static/media/contact_illustration.476f3fd6.png"
+  },
+  {
+    "revision": "b2cfe9b696cab044de141841903701f7",
+    "url": "/static/media/hamburger_hover_icon.b2cfe9b6.svg"
+  },
+  {
+    "revision": "a399be22ee3090784b2dc33985127f4d",
+    "url": "/static/media/hamburger_icon.a399be22.svg"
+  },
+  {
+    "revision": "9b53a2171793678875e5ad7d51c70b5f",
+    "url": "/static/media/juniorsjourneybanner.9b53a217.png"
+  },
+  {
+    "revision": "9351353eb52c934ecbe29669de8c666c",
+    "url": "/static/media/logo.9351353e.svg"
+  },
+  {
+    "revision": "57342cb4ffe133c3158ee0e5457a0180",
+    "url": "/static/media/logo_grey.57342cb4.svg"
+  },
+  {
+    "revision": "71b80df53547046a953a38cb795e4699",
+    "url": "/static/media/parliamentDrawing.71b80df5.png"
+  },
+  {
+    "revision": "9efd0caace424db30d9a17a81a475475",
+    "url": "/static/media/portrait.9efd0caa.jpg"
+  },
+  {
+    "revision": "0f3472609de9264785bea5128b202161",
+    "url": "/static/media/profile_pic_opacity.0f347260.png"
+  }
+]);
